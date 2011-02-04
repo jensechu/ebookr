52c6da3599af5d7e5a4b49a199caf60676c21829
Jensen Hussey <jensen@jensechu.com>
2011-02-04 06:17:22 -0500

    Cleaned up the python with a regular expression for replacing double quotes with LaTeX commands

--- text/test.py
+++ text/test.py
@@ -1,38 +1,40 @@
 import codecs
+import re
 
 #Input filename
 prompt = "Filename: "
-z = raw_input(prompt)
+file_name = raw_input(prompt)
 
 #Extract text and close the file
-book_open = codecs.open(z, "r+", "utf-8")
-book = book_open.read()
+book_open = codecs.open(file_name, "r+", "utf-8")
+book_read = book_open.read()
 book_open.close()
 
+
+
 #Checks if file has already been ebooked.
-def check(a):
-    search = a.find("ebooked")    
+def check(booked):
+    search = booked.find("ebooked")    
     if search == -1:
-        decoder(a)
+        decoder(booked)
         print "REPLACED YOUR STUFF"
     else:
         print "Your book has already been ebooked."
 
+
+
 #LaTeX character replacements
-def decoder(a):
-    #Verifies check for future
-    book_add = codecs.open(z, "a", "utf-8")
-    book_add.write("% ebooked") 
-
-    #Characters
-    double = "'"
-
-    #Replacement
-    a = a.replace("'", "\\textquotedblleft{}")
-    print a
+def decoder(book_new):
+    #Replace double quotes
+    double_quotes = re.compile(r'"(.+?)"', re.MULTILINE | re.DOTALL)
+    book_new = double_quotes.sub(r'\\textquotedblleft \1\\textquotedblright \\textcolor{White}{a} ', book_new)
 
     #Write new code to file
-    book_open = codecs.open(z, "w", "utf-8")
-    book_open.write(a)
+    book_open = codecs.open(file_name, "w", "utf-8")
+    book_open.write(book_new)
 
-check(book)
+    #Makes it false for the check() next time
+    book_add = codecs.open(file_name, "a", "utf-8")
+    book_add.write("% ebooked") 
+
+check(book_read)
