dbcc3dc9112173f63304d59d4619bc833ea8571d
Jensen Hussey <jensechu@gmail.com>
2011-03-20 05:03:05 -0400

    Added a check for args

--- text/test.py
+++ text/test.py
@@ -1,18 +1,19 @@
+from sys import argv
 import codecs
 import re
 
-#Input filename
-prompt = "Filename: "
-file_name = raw_input(prompt)
+# Prompts for a file name if none is given.
+if len(argv) != 2:
+    file_name = raw_input("File name: ")
+else:
+    file_name = argv[1]
 
-#Extract text and close the file
+# Extract text and close the file
 book_open = codecs.open(file_name, "r+", "utf-8")
 book_read = book_open.read()
 book_open.close()
 
-
-
-#Checks if file has already been ebooked.
+# Checks if file has already been ebooked.
 def check(booked):
     search = booked.find("ebooked")    
     if search == -1:
@@ -21,19 +22,16 @@ def check(booked):
     else:
         print "Your book has already been ebooked."
 
-
-
-#LaTeX character replacements
+# LaTeX character replacements
 def decoder(book_new):
-    #Replace double quotes
+    # Replace double quotes
     double_quotes = re.compile(r'"(.+?)"', re.MULTILINE | re.DOTALL)
     book_new = double_quotes.sub(r'\\textquotedblleft \1\\textquotedblright \\textcolor{White}{a} ', book_new)
-
-    #Write new code to file
+    # Write new code to file
     book_open = codecs.open(file_name, "w", "utf-8")
     book_open.write(book_new)
 
-    #Makes it false for the check() next time
+    # Makes it false for the check() next time
     book_add = codecs.open(file_name, "a", "utf-8")
     book_add.write("% ebooked") 
 
