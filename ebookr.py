3e4dfa81fe0648ad825aeb706ac7cd54efb8b986
Jensen Hussey <jensechu@gmail.com>
2011-03-20 10:17:56 -0400

    Fixed a small bug, all good now. :D

--- ebookr.py
+++ ebookr.py
@@ -13,16 +13,14 @@ book_open = codecs.open(file_name, "r+", "utf-8")
 book_read = book_open.read()
 book_open.close()
 
-# Checks if file has already been ebooked.
+# Checks if file has already been ebooked
 def check(booked):
     search = booked.find("ebooked")    
     if search == -1:
-        print "REPLACING YOUR STUFF."
         decoder(booked)
         print "REPLACED YOUR STUFF"
     else:
         print "Your book has already been ebooked."
-        create_tex(file_name)
 
 # Creates a .pdf with LaTeX from the ebooked file_name
 def create_tex(ebooked):
@@ -31,7 +29,7 @@ def create_tex(ebooked):
 
 # LaTeX character replacements
 def decoder(book_new):
-    # TODO: Adds the LaTeX \begin{document} and preamble
+    # Adds the LaTeX preamble and closer to the book
     latex_preamble = '''
 \documentclass[12pt]{book} \n \n
 
@@ -53,30 +51,19 @@ def decoder(book_new):
 \let\cleardoublepage\clearpage
 
 '''
-    print latex_preamble
-    book_tex = latex_preamble + book_new + "\n\end{document}"
-    print book_tex 
-    print "This is book_tek1"
-    #exit(0)
+    book_tex = latex_preamble + book_new + "\n \end{document}"
 
     # Replace double quotes
     double_quotes = re.compile(r'"(.+?)"', re.MULTILINE | re.DOTALL)
-    book_tex = double_quotes.sub(r'\\textquotedblleft \1\\textquotedblright \\textcolor{White}{a} ', book_tex)
-    print book_tex 
-    print "this is tex2"
+    book_tex = double_quotes.sub(r'\\textquotedblleft \\textquotedblright \\textcolor{White}{a} ', book_tex)
 
     # Write new code to file
     book_open = codecs.open(file_name, "w", "utf-8")
     book_open.write(book_tex)
 
-    print book_open
-
     # Makes it false for the check() next time
     book_add = codecs.open(file_name, "a", "utf-8")
     book_add.write("% ebooked") 
-
-    #create_tex(file_name)
-
 
 check(book_read)
 create_tex(file_name)
